ff9d3eb1b7a4a6ac83da21714189f68c0f83524c
Muetschki <xmahlknecht@tsn.at>
2025-05-23 09:32:01 +0200

    Progress html funktioniert

--- php/db.php
+++ php/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db   = 'sprachlerner';
 $user = 'root';
-$pass = '';
+$pass = 'root';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
--- php/get_progress.php
+++ php/get_progress.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 session_start();
 require_once 'db.php';
 
